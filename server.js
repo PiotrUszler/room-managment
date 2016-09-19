33f3e58809ab1a2b2d06677843c8497a7d8c0514
Piotr Uszler <xothas@gmail.com>
2016-09-19 16:55:20 +0200

    .

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by Piotr Uszler on 19.09.2016.
  */
+var express = require('express'),
+    app = express();
+
+app.listen(process.env.PORT || 3000, function () {
+    if(process.env.PORT != undefined)
+        console.log('Server działa na procie ' + process.env.PORT);
+    else
+        console.log('Server działa na procie 3000');
+});
