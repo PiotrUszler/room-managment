7c28fec6b4ed9dafe20cc0e6fc93375812dfa83f
Piotr Uszler <xothas@gmail.com>
2017-01-25 11:47:07 +0100

    .

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-/**
+8/**
  * Created by Piotr Uszler on 19.09.2016.
  */
-var mongoose = require('mongoose');
 
+var mongoose = require('mongoose');
+/*
 mongoose.connect('mongodb://localhost:27017/room-managment-system', function () {
     console.log('Połączono z db')
 });
 
-/*
+*/
 mongoose.connect('mongodb://Shardo:Piotrek123@ds147985.mlab.com:47985/mytestdb', function () {
     console.log('Połączono z db')
 });
-*/
+
 module.exports = mongoose;
